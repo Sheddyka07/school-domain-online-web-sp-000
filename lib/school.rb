5025f7e13671ecec80df0e5f59d1268b1c1bc87f
Kelly Sheddy <jungaka07@gmail.com>
2018-11-28 13:25:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+  ROSTER = {}
+  
+  def initialize(school)
+    @school = school
+  end
+  
+  def add_student(name, grade)
+    roster[name] = []
+    roster[name] << grade
+    roster
+  end 
+  
+end
+
+
+
